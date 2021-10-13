38aecb78a5fc2ed80a250c6f1d47cdd4fcf2f407
= <=>
2021-10-13 16:26:27 -0500

    Se suben nuevos ajustes  configuracion grillas

---
+++ app/Model/ConfiguracionesEspecialesDocumentosDet.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ConfiguracionesEspecialesDocumentosDet extends Model
+{
+    protected $table="configuraciones_especiales_documentos_det";
+
+    protected $primaryKey ="Id";
+}
