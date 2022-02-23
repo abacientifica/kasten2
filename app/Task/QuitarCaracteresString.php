6f94b14305c711bdd8299df64d694344af3c98bd
= <=>
2022-02-23 09:46:40 -0500

    metodo modificar caracteres

--- app/Task/QuitarCaracteresString.php
+++ app/Task/QuitarCaracteresString.php
@@ -13,11 +13,11 @@ class QuitarCaracteresString {
                 $no_permitidas = array("á", "é", "í", "ó", "ú", "Á", "É", "Í", "Ó", "Ú", "ñ", "À", "Ã", "Ì", "Ò", "Ù", "Ã™", "Ã ", "Ã¨", "Ã¬", "Ã²", "Ã¹", "ç", "Ç", "Ã¢", "ê", "Ã®", "Ã´", "Ã»", "Ã‚", "ÃŠ", "ÃŽ", "Ã”", "Ã›", "ü", "Ã¶", "Ã–", "Ã¯", "Ã¤", "«", "Ò", "Ã", "Ã„", "Ã‹", "Ñ","'",'"');
                 $permitidas = array("a", "e", "i", "o", "u", "A", "E", "I", "O", "U", "n", "N", "A", "E", "I", "O", "U", "a", "e", "i", "o", "u", "c", "C", "a", "e", "i", "o", "u", "A", "E", "I", "O", "U", "u", "o", "O", "i", "a", "e", "U", "I", "A", "E", "N","","");
                 $item = str_replace($no_permitidas, $permitidas, $item);
-                return preg_replace('([^A-Za-z0-9 ])', ' ', $item);
+                return ($item);
             }
             return $item;
         };
-        $data = is_array($data) ? array_map($EliminarCaracteres,$data) : $EliminarCaracteres($data);
+        $data =  $EliminarCaracteres($data);
         return $next($data);
     }
 }
